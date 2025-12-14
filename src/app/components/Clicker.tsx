212d33282ae470106010fcd8349be08d8bb24adf
JaytirthJOSHI <jaytirthjayjoshi@gmail.com>
2025-12-13 23:54:59 -0500

    ok ok quick PoC so i can ship

--- src/app/components/Clicker.tsx
+++ src/app/components/Clicker.tsx
@@ -9,7 +9,7 @@ export default function Clicker ({onClick}: ClickerProps)
 {
 return (
     <button onClick={onClick}>
-    <image src="/GUAC.jpg" alt="festive guacamol and some other cool stuff fr fr just tust me" width={512} height={512}/>
+    <Image src="/guac_img_geminimade_tee_hee.png" alt="festive guacamol and some other cool stuff fr fr just tust me" width={512} height={512}/>
     </button>
 );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,28 @@
-import { MyFont } from "next/font/google";
+import type { Metadata } from "next";
+import { Jolly_Lodger } from "next/font/google";
+import "./globals.css";
 
-const myFont = MyFont({
+const myFont = Jolly_Lodger({
 	subsets: ["latin"],
-	weight: "variable", 
+	weight: "400", 
 	variable: "--Jolly-Lodger",
-})
+});
+
+export const metadata: Metadata = {
+  title: "Haxmas Day 1",
+  description: "Christmas Clicker Game... Or something like that lmao jk jk",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${myFont.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
